15e4fe280bc8daef1aa2f717d3d86ac723ed0d9a
Gil Neto <gil@opplane.com>
2020-11-17 21:06:54 +0000

    added 'locks' table and validators to both this and 'schema_meta' tables. migrations next.

--- kong/db/strategies/mongo/connector.lua
+++ kong/db/strategies/mongo/connector.lua
@@ -35,8 +35,12 @@ local function get_server_info(client, database)
   return assert(client:command(database, '{"buildInfo":1}')):value()
 end
 
-local function get_collection_names(connection)
-  return assert(connection:getCollectionNames())
+local function get_collection_names(connection, database)
+  local db, err = assert(connection:getDatabase(database))
+  if not db then
+    return nil, err
+  end
+  return assert(db:getCollectionNames())
 end
 
 local function get_collection(connection, name)
@@ -48,8 +52,13 @@ local function get_collection(connection, name)
   return assert(collection:find(query))
 end
 
-local function create_collection(connection, name, opts)
-  return assert(connection:createCollection(name, opts))
+local function create_collection(connection, database, name, opts)
+  local db = connection:getDatabase(database)
+  return assert(db:createCollection(name, opts))
+end
+
+local function create_index(connection, database, index_query)
+  return assert(connection:command(database, mongo.BSON(index_query)))
 end
 
 function MongoConnector.new(kong_config)
@@ -126,7 +135,7 @@ function MongoConnector:init()
   return true
 end
 
-function MongoConnector:init_worker(strategies)
+function MongoConnector:init_worker(_)
   -- still have to understand wtf this does
   print('(MongoConnector.init_worker) To Do')
 end
@@ -156,11 +165,9 @@ function MongoConnector:connect()
     return nil, err
   end
 
-  local connection = client:getDatabase(self.config.database)
+  self:store_connection(client)
 
-  self:store_connection(connection)
-
-  return connection
+  return client
 end
 
 function MongoConnector:connect_migrations()
@@ -180,6 +187,65 @@ function MongoConnector:close()
   return true
 end
 
+function MongoConnector:setup_locks(_,_)
+  local conn = self:get_stored_connection()
+  if not conn then
+    error("no connection")
+  end
+
+  log.debug("creating 'locks' table if not existing...")
+  local coll, err = create_collection(conn, self.config.database, 'locks', [[{
+      "validator": {
+        "$jsonSchema": {
+          "bsonType": "object",
+          "required": ["key"],
+          "properties": {
+            "key": { "bsonType": "string" },
+            "owner": { "bsonType": "string" },
+            "ttl": { "bsonType": "timestamp" }
+          }
+        }
+      }
+    }]])
+  if not coll then
+    return nil, err
+  end
+
+  local query = fmt([[{
+      "createIndexes": "locks",
+      "indexes": [
+        { "key": { "ttl": 1 }, "name": "locks_ttl_idx" }
+      ]
+    }]], SCHEMA_META_KEY)
+  local idx
+  idx, err = create_index(conn, self.config.database, query)
+  if not idx then
+    return nil, err
+  end
+
+  logger.debug("successfully created 'locks' table")
+
+  return true
+end
+
+function MongoConnector:insert_lock(key, ttl, owner)
+  -- TODO
+
+  return true
+end
+
+function MongoConnector:read_lock(key)
+  -- TODO
+
+  return true
+end
+
+function MongoConnector:remove_lock(key, owner)
+  -- TODO
+
+  return true
+end
+
 do
 
   local SCHEMA_META_KEY = "schema_meta"
@@ -190,7 +256,7 @@ do
       error("no connection")
     end
 
-    local table_names, err = get_collection_names(conn)
+    local table_names, err = get_collection_names(conn, self.config.database)
     if not table_names then
       return nil, err
     end
@@ -208,12 +274,10 @@ do
       return nil
     end
 
-    local conn = self:get_stored_connection()
-    if not conn then
-      conn = self:connect()
-    end
+    local db = conn:getDatabase(self.config.database)
 
-    local records, err = get_collection(conn, SCHEMA_META_KEY)
+    local records
+    records, err = get_collection(db, SCHEMA_META_KEY)
     if not records then
       return nil, err
     end
@@ -232,19 +296,56 @@ do
     return records
   end
 
-  function MongoConnector:schema_bootstrap()
+  function MongoConnector:schema_bootstrap(_, default_locks_ttl)
     local conn = self:get_stored_connection()
     if not conn then
       error("no connection")
     end
 
-    -- TODO add validator - postgres/connector.lua l.783
-    -- TODO add auth - postgres/connector.lua l.759
-    local coll, err = create_collection(conn, SCHEMA_META_KEY, mongo.BSON '{}')
+    -- TODO add authz - postgres/connector.lua l.759
+
+    local coll, err = create_collection(conn, self.config.database, SCHEMA_META_KEY, [[{
+      "validator": {
+        "$jsonSchema": {
+          "bsonType": "object",
+          "required": ["key","subsystem"],
+          "properties": {
+            "key": { "bsonType": "string" },
+            "subsystem": { "bsonType": "string" },
+            "last_executed": { "bsonType": "string" },
+            "executed": { "bsonType": "array", "items": { "bsonType": "string" } },
+            "pending": { "bsonType": "array", "items": { "bsonType": "string" } }
+          }
+        }
+      }
+    }]])
     if not coll then
       return nil, err
     end
 
+    log.debug(fmt("successfully created %s collection", SCHEMA_META_KEY))
+
+    local query = fmt([[{
+      "createIndexes": "%s",
+      "indexes": [
+        { "key": { "key": 1, "subsystem": 1 }, "name": "primary_key", "unique": true }
+      ]
+    }]], SCHEMA_META_KEY)
+    local idx
+    idx, err = create_index(conn, self.config.database, query)
+    if not idx then
+      return nil, err
+    end
+
+    log.debug(fmt("set 'key' and 'subsystem' as primary key for %s", SCHEMA_META_KEY))
+
+    local ok
+    ok, err = self:setup_locks(default_locks_ttl, true)
+    if not ok then
+      return nil, err
+    end
+
+    return true
   end
 
 end
